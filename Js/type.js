4a22c890988a47346ab33f91b0dc71b4e026be1a
nifemisanni <nifemisanni60@gmail.com>
2022-05-18 22:04:18 +0100

    Updated VP

---
+++ Js/type.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typed", {
+  // Waits 1000ms after typing "First"
+  strings: ["First ^1000 sentence.", "Second sentence."],
+});
+
+// import Typed from "typed.js";
+
+// var options = {
+//   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+//   typeSpeed: 40,
+// };
+
+// var typed = new Typed(".", options);
